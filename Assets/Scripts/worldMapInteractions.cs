ec7e292fdc0fd00bcb8580ce44d3d9b8e4aadb60
ShaneStrander <shanestrander@gmail.com>
2022-11-12 11:17:10 -0600

    Select map region

--- Assets/Scripts/worldMapInteractions.cs
+++ Assets/Scripts/worldMapInteractions.cs
@@ -8,16 +8,25 @@ public class worldMapInteractions : MonoBehaviour
     private Color basicColor = Color.green;
     private Color hoverColor = new Color(107f / 255f, 255f / 255f, 107f / 255f);
 
+    public string selectedRegion = "";
+
+    private Camera camera;
+
     // Start is called before the first frame update
     void Start()
     {
         GetComponent<SpriteRenderer>().color = basicColor;
+        camera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetMouseButtonDown(0))
+        {
+            CastRay();
+            Debug.Log(selectedRegion);
+        }
     }
 
     void OnMouseOver()
@@ -29,4 +38,15 @@ public class worldMapInteractions : MonoBehaviour
     {
         GetComponent<SpriteRenderer>().color = basicColor;
     }
+
+    void CastRay()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
+        if (hit.collider != null)
+        {
+            selectedRegion = (hit.collider.gameObject.name);
+        }
+    }
+
 }
