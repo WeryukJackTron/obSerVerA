5b19a9ca2ccc5187120f60be13db32ce21d9ce36
Petter Tveit <petter.tveit@lyse.net>
2022-12-05 22:25:12 +0100

    changed log in tutorial

--- Assets/Scripts/DisableEnable.cs
+++ Assets/Scripts/DisableEnable.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class DisableEnable : MonoBehaviour
 {
     public GameObject infoPanel;
+    public float sec = 1f;
 
     public void onButtonClick()
-    {
+    {   
+        //StartCoroutine(AnimationFirst(sec));
+
         if (infoPanel.activeInHierarchy == true)
         {
             infoPanel.SetActive(false);
@@ -17,4 +20,18 @@ public class DisableEnable : MonoBehaviour
             infoPanel.SetActive(true);
         }
     }
+    /*
+    public IEnumerator AnimationFirst(float seconds)
+    {
+        if (infoPanel.activeInHierarchy == true)
+        {
+            yield return new WaitForSeconds(seconds);
+            infoPanel.SetActive(false);
+        }
+        else
+        {
+            infoPanel.SetActive(true);
+        }
+    }
+    */
 }
