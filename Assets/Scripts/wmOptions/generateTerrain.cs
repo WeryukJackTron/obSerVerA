069908a637b0c08917175e7ad1f098eac4e5ded1
ShaneStrander <shanestrander@gmail.com>
2022-12-05 05:53:58 -0600

    perlin noise updates

--- Assets/Scripts/wmOptions/generateTerrain.cs
+++ Assets/Scripts/wmOptions/generateTerrain.cs
@@ -13,19 +13,31 @@ public class generateTerrain : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        populateTiles();
+    }
+
+
+    void populateTiles()
+    {
         for (int x = 0; x < 89; x++)
         {
             for (int y = 0; y < 89; y++)
             {
-                float noise = GetNoiseAt(x, y, 1.0f, 1.0f, 0.5f, 1.0f);
-                if (noise < 0.4f)
+                float noise = perlinNoise(x, y, 1.0f, 1.0f, 0.5f);
+                if (noise < 0.3f)
                 {
                     Vector3Int p = new Vector3Int(x, y, 0);
                     Tile tile = ScriptableObject.CreateInstance<Tile>();
                     tile.sprite = trees[Random.Range(0, trees.Length)];
                     tilemap.SetTile(p, tile);
                 }
-                Debug.Log(noise);
+                else if (noise > 0.8f)
+                {
+                    Vector3Int p = new Vector3Int(x, y, 0);
+                    Tile tile = ScriptableObject.CreateInstance<Tile>();
+                    tile.sprite = mountains[Random.Range(0, mountains.Length)];
+                    tilemap.SetTile(p, tile);
+                }
             }
         }
     }
@@ -33,20 +45,17 @@ public class generateTerrain : MonoBehaviour
     /// scale : The scale of the "perlin noise" view
     /// heightMultiplier : The maximum height of the terrain
     /// persistance : The higher it is, the rougher the terrain will be (this value should be between 0 and 1 excluded)
-    /// lacunarity : The higher it is, the more "feature" the terrain will have (should be strictly positive)
-    public static float GetNoiseAt(int x, int z, float scale, float heightMultiplier, float persistance, float lacunarity)
+    public static float perlinNoise(int x, int y, float scale, float heightMultiplier, float persistance)
     {
         float PerlinValue = 0f;
         float amplitude = 1f;
-        float frequency = 0.1f;
 
 
         // Get the perlin value at that octave and add it to the sum
-        PerlinValue += Mathf.PerlinNoise(x * frequency, z * frequency) * amplitude;
+        PerlinValue += Mathf.PerlinNoise(x * 0.1f, y * 0.1f) * amplitude;
 
         // Decrease the amplitude and the frequency
         amplitude *= persistance;
-        frequency *= lacunarity;
 
 
         // Return the noise value
