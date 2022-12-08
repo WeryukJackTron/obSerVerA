9c45a780974011199bda30f5efea6a76cc863b38
Petter Tveit <petter.tveit@lyse.net>
2022-12-08 14:42:23 +0100

    volume slider fix

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -2,14 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
 {
 
     public AudioMixer audioMixer;
+    public Slider slider;
     
-    public void SetVolume(float volume)
+    void Start()
     {
-        audioMixer.SetFloat("volume", volume);
+        slider.value = PlayerPrefs.GetFloat("volume", 0.75f);
+    }
+
+    public void SetVolume()
+    {
+        float sliderValue = slider.value;
+        audioMixer.SetFloat("volume", Mathf.Log10 (sliderValue) * 20);
+        PlayerPrefs.SetFloat("volume", sliderValue);
     }
 }
